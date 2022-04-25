507e61c34cb0cd7bf8fca954a480c6f4ca3ba00d
datamaster04 <datamaster04@gmail.com>
2022-04-25 13:13:34 +0200

    Update Nyheter.jsx

--- liverpool-hamkam-fanpage/src/pages/Nyheter.jsx
+++ liverpool-hamkam-fanpage/src/pages/Nyheter.jsx
@@ -5,23 +5,19 @@ import førsteseier from "../assets/images/førsteseier.jpg";
 const OmOss = () => {
   return (
     <>
-      
       <div className="nyheter_header">Nyheter</div>
 
       <div className="nyheter_box">
         <div className="nyhet_førsteseier">
-          <img 
-          className="førstesier_bilde"
-          src={førsteseier}
-          alt="HamKam - Sandefjord 18.04.22"
+          <img
+            className="førstesier_bilde"
+            src={førsteseier}
+            alt="HamKam - Sandefjord 18.04.22"
           />
-          <p className="førsteseier_header"> Taktsik grep ga 3 poeng</p>
-          <div className="førsteseier_text">
-
-          </div>
+          <a className="førsteseier_header"> Taktsik grep ga 3 poeng</a>
+          <div className="førsteseier_text"></div>
         </div>
       </div>
-
     </>
   );
 };
