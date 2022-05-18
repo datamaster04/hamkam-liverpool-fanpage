90c8fd224e0b7bdef1d1027e656eaa5b1ff19fb0
datamaster04 <datamaster04@gmail.com>
2022-05-18 09:41:34 +0200

    Update App.js

--- liverpool-hamkam-fanpage/src/App.js
+++ liverpool-hamkam-fanpage/src/App.js
@@ -7,7 +7,7 @@ import Kamper from './pages/Kamper'
 import Nyheter from './pages/Nyheter'
 import OmOss from './pages/OmOss'
 import OvergangerOgRykter from './pages/OvergangerOgRykter'
-import Førsteseier from './nyheter_pages/Førsteseier'
+import Førsteseier from './nyheter_pages/førsteseier'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from "./components/Header"
